704dd27f12d138c398321020bf5f0a208e7dd2da
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-02 22:03:35 +0200

    Make a text field and submit buttom for the query

---
+++ src/Components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>AnalyzAI</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/querybox.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import '../App.css'
+
+const QueryBox = () => {
+  const [query, setQuery] = useState('')
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log('Query:'  + query)
+
+  }
+
+  // TODO: Implement file upload and query handling logic
+
+  // return (
+  //   <div>
+  //     <form onSubmit={handleSubmit}>
+  //     <div id='textbox'>
+  //       <input
+  //         type='text'
+  //         placeholder='Enter a query'
+  //         id='query-input'
+  //         value={query}
+  //         onChange={handleQueryChange}
+  //       />
+  //       <button type='submit' id='submit-button'>
+  //         Submit
+  //       </button>
+  //     </div>
+  //     </form>
+  //   </div>
+  // )
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div id='textbox'>
+          <input
+            type='text'
+            placeholder='Enter a query'
+            id='query-input'
+            value={query}
+            onChange={handleQueryChange}
+          />
+          <button type='submit' id='submit-button'>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+  
+
+  
+}
+
+export default QueryBox
