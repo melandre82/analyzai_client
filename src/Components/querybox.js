abc7d3d575c1d5f7fc44a4f532c8eaafb595e145
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-19 16:29:29 +0200

    Prevent user from submitting empty query

--- src/Components/querybox.js
+++ src/Components/querybox.js
@@ -1,61 +1,3 @@
-// import React, { useState } from 'react'
-// import '../App.css'
-// import axios from 'axios'
-
-// const QueryBox = ({ setData }) => {
-//   const [query, setQuery] = useState('')
-
-//   const handleQueryChange = (event) => {
-//     setQuery(event.target.value)
-//   }
-
-//   const handleSubmit = async (event) => {
-//     try {
-//       event.preventDefault()
-
-//       const body = {
-//         query: query,
-//       }
-
-//       await axios
-//         .post('http://localhost:5001/query', body)
-//         .then((response) => {
-//           console.log(response.data)
-//           console.log('typeof: ' + typeof response.data)
-//           setData(response.data.text);
-//         })
-//         .catch((error) => {
-//           return console.error('Error fetching data:', error)
-//         })
-
-//         setQuery('')
-//       // console.log('Query:' + query)
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <div id='textbox'>
-//           <input
-//             type='text'
-//             placeholder='Enter a query'
-//             id='query-input'
-//             value={query}
-//             onChange={handleQueryChange}
-//           />
-//           <button type='submit' id='submit-button'>
-//             Submit
-//           </button>
-//         </div>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default QueryBox
 import React, { useState } from 'react';
 import '../App.css';
 import axios from 'axios';
@@ -73,7 +15,7 @@ const QueryBox = ({ setData }) => {
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
-      setLoading(true); // Set loading to true to show the loading sign
+      setLoading(true);
 
       const body = {
         query: query,
@@ -90,7 +32,7 @@ const QueryBox = ({ setData }) => {
           console.error('Error fetching data:', error);
         })
         .finally(() => {
-          setLoading(false); // Set loading back to false after receiving the response
+          setLoading(false);
           setQuery('');
         });
     } catch (error) {
@@ -109,9 +51,8 @@ const QueryBox = ({ setData }) => {
             value={query}
             onChange={handleQueryChange}
           />
-          <button type='submit' id='submit-button' disabled={loading}
-          // disabled={loading}
-          className={loading ? 'loading' : ''}
+          <button type='submit' id='submit-button' disabled={loading || !query}
+          className={loading || !query ? 'loading' : ''}
           >
             
             {loading ? (
