2ba223de477a732b5eed289eb7fc6c97c66961b5
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-19 16:03:30 +0200

    Display loading sign when ssending query

--- src/Components/querybox.js
+++ src/Components/querybox.js
@@ -1,39 +1,102 @@
-import React, { useState } from 'react'
-import '../App.css'
-import axios from 'axios'
+// import React, { useState } from 'react'
+// import '../App.css'
+// import axios from 'axios'
+
+// const QueryBox = ({ setData }) => {
+//   const [query, setQuery] = useState('')
+
+//   const handleQueryChange = (event) => {
+//     setQuery(event.target.value)
+//   }
+
+//   const handleSubmit = async (event) => {
+//     try {
+//       event.preventDefault()
+
+//       const body = {
+//         query: query,
+//       }
+
+//       await axios
+//         .post('http://localhost:5001/query', body)
+//         .then((response) => {
+//           console.log(response.data)
+//           console.log('typeof: ' + typeof response.data)
+//           setData(response.data.text);
+//         })
+//         .catch((error) => {
+//           return console.error('Error fetching data:', error)
+//         })
+
+//         setQuery('')
+//       // console.log('Query:' + query)
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <div id='textbox'>
+//           <input
+//             type='text'
+//             placeholder='Enter a query'
+//             id='query-input'
+//             value={query}
+//             onChange={handleQueryChange}
+//           />
+//           <button type='submit' id='submit-button'>
+//             Submit
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default QueryBox
+import React, { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const QueryBox = ({ setData }) => {
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleQueryChange = (event) => {
-    setQuery(event.target.value)
-  }
+    setQuery(event.target.value);
+  };
 
   const handleSubmit = async (event) => {
     try {
-      event.preventDefault()
+      event.preventDefault();
+      setLoading(true); // Set loading to true to show the loading sign
 
       const body = {
         query: query,
-      }
+      };
 
       await axios
         .post('http://localhost:5001/query', body)
         .then((response) => {
-          console.log(response.data)
-          console.log('typeof: ' + typeof response.data)
+          console.log(response.data);
+          console.log('typeof: ' + typeof response.data);
           setData(response.data.text);
         })
         .catch((error) => {
-          return console.error('Error fetching data:', error)
+          console.error('Error fetching data:', error);
         })
-
-        setQuery('')
-      // console.log('Query:' + query)
+        .finally(() => {
+          setLoading(false); // Set loading back to false after receiving the response
+          setQuery('');
+        });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <div>
@@ -46,13 +109,22 @@ const QueryBox = ({ setData }) => {
             value={query}
             onChange={handleQueryChange}
           />
-          <button type='submit' id='submit-button'>
-            Submit
+          <button type='submit' id='submit-button' disabled={loading}
+          // disabled={loading}
+          className={loading ? 'loading' : ''}
+          >
+            
+            {loading ? (
+              <FontAwesomeIcon icon={faSpinner} spin />
+            ) : (
+              'Submit'
+            )}
           </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default QueryBox
+export default QueryBox;
+
