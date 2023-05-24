8eb998e3e67aef0707e6a02444cb9bda93f1625c
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-24 16:20:33 +0200

    Make pdf nav bar not look terrible

--- src/Components/uploadedFiles.js
+++ src/Components/uploadedFiles.js
@@ -47,8 +47,8 @@ export default function UploadedFiles() {
   }
 
   function resetScale() {
-    setScale(1.5);
-}
+    setScale(1.5)
+  }
 
   useEffect(() => {
     localForage.iterate((value, key) => {
@@ -59,104 +59,107 @@ export default function UploadedFiles() {
   const isLoading = renderedPageNumber !== pageNumber || renderedScale !== scale
 
   return (
-    <div className='uploaded-files-container'>
-      <div className='file-bar'>
-        <ul className='file-list'>
-          {files.map(({ name, file }, index) => (
-            <li key={index} onClick={() => setCurrentFile(file)}>
-              <img
-                class='icon'
-                src={require('../icons/icons8-pdf-96.png')}
-                alt='Icon'
-              />{' '}
-              {name}
-            </li>
-          ))}
-        </ul>
+    <div>
+      <div className='navigation-box'>
+        <div className='navigation'>
+          {/* <div className='page-controller'> */}
+            <div className='page-number'>
+              <p>
+                {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
+              </p>
+            </div>
+            <button
+              type='button'
+              className='previous'
+              disabled={pageNumber <= 1}
+              onClick={previousPage}
+            >
+              &lt;
+            </button>{' '}
+            <button
+              type='button'
+              className='next'
+              disabled={pageNumber >= numPages}
+              onClick={nextPage}
+            >
+              &gt;
+            </button>
+          {/* </div> */}
+          {/* <div className='size-control'> */}
+            <button type='button' className='reset' onClick={resetScale}>
+              {Math.round((scale * 100) / 1.5)}%
+            </button>
+            <button
+              type='button'
+              className='minus'
+              disabled={scale <= 1}
+              onClick={decreaseScale}
+            >
+              -
+            </button>{' '}
+            <input
+              type='range'
+              min='1'
+              max='3'
+              value={scale}
+              onChange={(event) => setScale(Number(event.target.value))}
+              step='0.1'
+            />{' '}
+            <button
+              type='button'
+              className='plus'
+              disabled={scale >= 3}
+              onClick={increaseScale}
+            >
+              +
+            </button>
+          {/* </div> */}
+        </div>
       </div>
+      <div className='uploaded-files-container'>
+        <div className='file-bar'>
+          <ul className='file-list'>
+            {files.map(({ name, file }, index) => (
+              <li key={index} onClick={() => setCurrentFile(file)}>
+                <img
+                  class='icon'
+                  src={require('../icons/icons8-pdf-96.png')}
+                  alt='Icon'
+                />{' '}
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      {currentFile && (
-        <div className='pdf-viewer'>
-          <div className='navigation'>
-            <div className='page-controller'>
-              <div className='page-number'>
-                <p>
-                  {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
-                </p>
-              </div>
-              <button
-                type='button'
-                className='previous'
-                disabled={pageNumber <= 1}
-                onClick={previousPage}
-              >
-                &lt;
-              </button>{' '}
-              <button
-                type='button'
-                className='next'
-                disabled={pageNumber >= numPages}
-                onClick={nextPage}
-              >
-                &gt;
-              </button>
-            </div>
-            <div className='size-control'>
-              <button type='button' className='reset' onClick={resetScale}>
-                {Math.round((scale * 100) / 1.5)}%
-              </button>
-              <button
-                type='button'
-                className='minus'
-                disabled={scale <= 1}
-                onClick={decreaseScale}
-              >
-                -
-              </button>{' '}
-              <input
-                type='range'
-                min='1'
-                max='3'
-                value={scale}
-                onChange={(event) => setScale(Number(event.target.value))}
-                step='0.1'
-              />{' '}
-              <button
-                type='button'
-                className='plus'
-                disabled={scale >= 3}
-                onClick={increaseScale}
-              >
-                +
-              </button>
-            </div>
-          </div>
-
-          <Document file={currentFile} onLoadSuccess={onDocumentLoadSuccess}>
-            {isLoading && renderedPageNumber && renderedScale ? (
+        {currentFile && (
+          <div className='pdf-viewer'>
+            <Document file={currentFile} onLoadSuccess={onDocumentLoadSuccess}>
+              {isLoading && renderedPageNumber && renderedScale ? (
+                <Page
+                  key={renderedPageNumber + '@' + renderedScale}
+                  className='prevPage'
+                  renderTextLayer={true}
+                  pageNumber={renderedPageNumber}
+                  scale={renderedScale}
+                  width={400}
+                />
+              ) : null}
               <Page
-                key={renderedPageNumber + '@' + renderedScale}
-                className='prevPage'
+                key={pageNumber + '@' + scale}
+                pageNumber={pageNumber}
                 renderTextLayer={true}
-                pageNumber={renderedPageNumber}
-                scale={renderedScale}
+                onRenderSuccess={() => {
+                  setRenderedPageNumber(pageNumber)
+                  setRenderedScale(scale)
+                }}
+                scale={scale}
                 width={400}
               />
-            ) : null}
-            <Page
-              key={pageNumber + '@' + scale}
-              pageNumber={pageNumber}
-              renderTextLayer={true}
-              onRenderSuccess={() => {
-                setRenderedPageNumber(pageNumber)
-                setRenderedScale(scale)
-              }}
-              scale={scale}
-              width={400}
-            />
-          </Document>
-        </div>
-      )}
+            </Document>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
