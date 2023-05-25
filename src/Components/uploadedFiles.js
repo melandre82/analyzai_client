1cc8a030e95c895dc2e098885b74207b3062593c
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-25 21:42:26 +0200

    Fetch user's pdfs from firestore

--- src/Components/uploadedFiles.js
+++ src/Components/uploadedFiles.js
@@ -1,24 +1,246 @@
-import { useState, useEffect } from 'react'
-import { pdfjs, Document, Page } from 'react-pdf'
-import localForage from 'localforage'
-import 'react-pdf/dist/esm/Page/TextLayer.css'
+// import { useState, useEffect } from 'react'
+// import { pdfjs, Document, Page } from 'react-pdf'
+// import localForage from 'localforage'
+// import 'react-pdf/dist/esm/Page/TextLayer.css'
 
-import '../CSS/uploadedFiles.css'
+// import '../CSS/uploadedFiles.css'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
+// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
 
-// solution from https://codesandbox.io/s/react-pdf-prevent-flash-with-scale-nse51l?file=/src/App.js:0-2717
+// // solution from https://codesandbox.io/s/react-pdf-prevent-flash-with-scale-nse51l?file=/src/App.js:0-2717
+
+// export default function UploadedFiles() {
+//   const [files, setFiles] = useState([])
+//   const [currentFile, setCurrentFile] = useState(null)
+//   const [numPages, setNumPages] = useState(null)
+//   const [pageNumber, setPageNumber] = useState(1)
+//   const [scale, setScale] = useState(1.5)
+//   const [renderedPageNumber, setRenderedPageNumber] = useState(null)
+//   const [renderedScale, setRenderedScale] = useState(null)
+//   const [inputValue, setInputValue] = useState(pageNumber);
+
+
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages)
+//   }
+
+//   function changePage(offset) {
+//     setPageNumber((prevPageNumber) => prevPageNumber + offset)
+//   }
+
+//   function previousPage() {
+//     changePage(-1)
+//   }
+
+//   function nextPage() {
+//     changePage(1)
+//   }
+
+//   function changeScale(offset) {
+//     setScale((prevScale) => prevScale + offset)
+//   }
+
+//   function decreaseScale() {
+//     changeScale(-0.1)
+//   }
+
+//   function increaseScale() {
+//     changeScale(0.1)
+//   }
+
+//   function resetScale() {
+//     setScale(1.5)
+//   }
+
+//   useEffect(() => {
+//     setInputValue(pageNumber);
+//   }, [pageNumber]);
+
+//   const handleInputChange = (e) => {
+//     setInputValue(e.target.value);
+//   }
+
+//   const handleKeyPress = (e) => {
+//     if (e.key === 'Enter') {
+//       const newPageNumber = parseInt(e.target.value);
+//       if (newPageNumber >= 1 && newPageNumber <= numPages) {
+//         setPageNumber(newPageNumber);
+//         e.target.blur();
+//       } else {
+//         setInputValue(pageNumber);  // Reset inputValue if it's not valid
+//       }
+//     }
+  
+//   }
+
+//   useEffect(() => {
+//     localForage.iterate((value, key) => {
+//       setFiles((files) => [...files, { name: key, file: value }])
+//     })
+//   }, [])
+
+//   const isLoading = renderedPageNumber !== pageNumber || renderedScale !== scale
+
+//   return (
+//     <div>
+//       <div className='navigation-box'>
+//         <div className='navigation'>
+//           {/* <div className='page-controller'> */}
+//           {/* <div className='page-number'>
+//               <p>
+//                 {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
+//               </p>
+//             </div> */}
+//          <div className='page-number'>
+//             <input
+//               type="text"
+//               value={inputValue}
+//               onClick={(event) => event.target.select()}
+//               onChange={handleInputChange}
+//               onKeyPress={handleKeyPress}
+//             />
+//             <span> / {numPages || '-'}</span>
+//             {/* <span> / {99999 || '-'}</span> */}
+//           </div>
+//           <button
+//             type='button'
+//             className='previous'
+//             disabled={pageNumber <= 1}
+//             onClick={previousPage}
+//           >
+//             &lt;
+//           </button>{' '}
+//           <button
+//             type='button'
+//             className='next'
+//             disabled={pageNumber >= numPages}
+//             onClick={nextPage}
+//           >
+//             &gt;
+//           </button>
+//           {/* </div> */}
+//           {/* <div className='size-control'> */}
+//           <button type='button' className='reset' onClick={resetScale}>
+//             {Math.round((scale * 100) / 1.5)}%
+//           </button>
+//           <button
+//             type='button'
+//             className='minus'
+//             disabled={scale <= 1}
+//             onClick={decreaseScale}
+//           >
+//             -
+//           </button>{' '}
+//           <input
+//             type='range'
+//             min='1'
+//             max='3'
+//             value={scale}
+//             onChange={(event) => setScale(Number(event.target.value))}
+//             step='0.1'
+//           />{' '}
+//           <button
+//             type='button'
+//             className='plus'
+//             disabled={scale >= 3}
+//             onClick={increaseScale}
+//           >
+//             +
+//           </button>
+//           {/* </div> */}
+//         </div>
+//       </div>
+//       <div className='uploaded-files-container'>
+//         <div className='file-bar'>
+//           <ul className='file-list'>
+//             {files.map(({ name, file }, index) => (
+//               <li key={index} onClick={() => setCurrentFile(file)}>
+//                 <img
+//                   className='icon'
+//                   src={require('../icons/icons8-pdf-96.png')}
+//                   alt='Icon'
+//                 />{' '}
+//                 {name}
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+
+//         {currentFile && (
+//           <div className='pdf-viewer'>
+//             <Document file={currentFile} onLoadSuccess={onDocumentLoadSuccess}>
+//               {isLoading && renderedPageNumber && renderedScale ? (
+//                 <Page
+//                   key={renderedPageNumber + '@' + renderedScale}
+//                   className='prevPage'
+//                   renderTextLayer={true}
+//                   pageNumber={renderedPageNumber}
+//                   scale={renderedScale}
+//                   width={400}
+//                 />
+//               ) : null}
+//               <Page
+//                 key={pageNumber + '@' + scale}
+//                 pageNumber={pageNumber}
+//                 renderTextLayer={true}
+//                 onRenderSuccess={() => {
+//                   setRenderedPageNumber(pageNumber)
+//                   setRenderedScale(scale)
+//                 }}
+//                 scale={scale}
+//                 width={400}
+//               />
+//             </Document>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
+
+import { useState, useEffect } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import { auth } from '../conf/firebase';
+import { onSnapshot, doc, collection } from "firebase/firestore";
+import { firestore } from '../conf/firebase';
+
+
+import '../CSS/uploadedFiles.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 export default function UploadedFiles() {
-  const [files, setFiles] = useState([])
-  const [currentFile, setCurrentFile] = useState(null)
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-  const [scale, setScale] = useState(1.5)
-  const [renderedPageNumber, setRenderedPageNumber] = useState(null)
-  const [renderedScale, setRenderedScale] = useState(null)
+  const [files, setFiles] = useState([]);
+  const [currentFile, setCurrentFile] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [scale, setScale] = useState(1.5);
+  const [renderedPageNumber, setRenderedPageNumber] = useState(null);
+  const [renderedScale, setRenderedScale] = useState(null);
   const [inputValue, setInputValue] = useState(pageNumber);
 
+
+  const user = auth.currentUser
+
+
+
+  
+  useEffect(() => {
+    const userId = user.uid;
+    const filesRef = collection(firestore, "users", userId, "files");
+    const unsubscribe = onSnapshot(filesRef, snapshot => {
+      const newFiles = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+  
+      setFiles(newFiles);
+    });
+  
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
@@ -70,28 +292,15 @@ export default function UploadedFiles() {
         setInputValue(pageNumber);  // Reset inputValue if it's not valid
       }
     }
-  
   }
 
-  useEffect(() => {
-    localForage.iterate((value, key) => {
-      setFiles((files) => [...files, { name: key, file: value }])
-    })
-  }, [])
-
-  const isLoading = renderedPageNumber !== pageNumber || renderedScale !== scale
+  const isLoading = renderedPageNumber !== pageNumber || renderedScale !== scale;
 
   return (
     <div>
       <div className='navigation-box'>
         <div className='navigation'>
-          {/* <div className='page-controller'> */}
-          {/* <div className='page-number'>
-              <p>
-                {pageNumber || (numPages ? 1 : '--')} / {numPages || '--'}
-              </p>
-            </div> */}
-         <div className='page-number'>
+          <div className='page-number'>
             <input
               type="text"
               value={inputValue}
@@ -100,7 +309,6 @@ export default function UploadedFiles() {
               onKeyPress={handleKeyPress}
             />
             <span> / {numPages || '-'}</span>
-            {/* <span> / {99999 || '-'}</span> */}
           </div>
           <button
             type='button'
@@ -118,8 +326,6 @@ export default function UploadedFiles() {
           >
             &gt;
           </button>
-          {/* </div> */}
-          {/* <div className='size-control'> */}
           <button type='button' className='reset' onClick={resetScale}>
             {Math.round((scale * 100) / 1.5)}%
           </button>
@@ -147,20 +353,19 @@ export default function UploadedFiles() {
           >
             +
           </button>
-          {/* </div> */}
         </div>
       </div>
       <div className='uploaded-files-container'>
         <div className='file-bar'>
           <ul className='file-list'>
-            {files.map(({ name, file }, index) => (
-              <li key={index} onClick={() => setCurrentFile(file)}>
+            {files.map(file => (
+              <li key={file.id} onClick={() => setCurrentFile(file.downloadURL)}>
                 <img
                   className='icon'
                   src={require('../icons/icons8-pdf-96.png')}
                   alt='Icon'
                 />{' '}
-                {name}
+                {file.fileName}
               </li>
             ))}
           </ul>
@@ -195,7 +400,6 @@ export default function UploadedFiles() {
         )}
       </div>
     </div>
-  )
+  );
 }
 
-// export default UploadedFiles
--- src/conf/firebase.js
+++ src/conf/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 import {getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,6 +21,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
+
+
+const db = getFirestore(app);
+
 export const auth = getAuth(app);
+export const firestore = db;
 
 const analytics = getAnalytics(app);
