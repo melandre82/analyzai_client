3878f6a625d5bb3e341baeaf6f8fda41e77a5676
ma225tq <ma225tq@student.lnu.se>
2024-08-04 23:38:26 +0200

    default scroll to bottom

--- src/Components/responsebox.js
+++ src/Components/responsebox.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const ResponseBox = ({ messages }) => {
+  const boxRef = useRef(null);
+  const [userScrolledUp, setUserScrolledUp] = useState(false);
+
   // This function aggregates consecutive server messages into single messages
   const aggregateMessages = (messages) => {
-    return messages.reduce((acc, message, index, array) => {
+    return messages.reduce((acc, message) => {
       const lastMessage = acc[acc.length - 1];
       if (lastMessage && lastMessage.type === 'server') {
         lastMessage.text = lastMessage.text || '';
@@ -19,8 +22,35 @@ const ResponseBox = ({ messages }) => {
 
   const aggregatedMessages = aggregateMessages(messages);
 
+  useEffect(() => {
+    const box = boxRef.current;
+
+    const handleScroll = () => {
+      // Check if the user has scrolled up
+      if (box.scrollTop + box.clientHeight < box.scrollHeight) {
+        setUserScrolledUp(true);
+      } else {
+        setUserScrolledUp(false);
+      }
+    };
+
+    box.addEventListener('scroll', handleScroll);
+
+    return () => {
+      box.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const box = boxRef.current;
+    // Scroll to the bottom if the user is not actively scrolling up
+    if (!userScrolledUp) {
+      box.scrollTop = box.scrollHeight;
+    }
+  }, [messages, userScrolledUp]);
+
   return (
-    <div className='box-container'>
+    <div className='box-container' ref={boxRef}>
       {aggregatedMessages.map((message, index) => (
         <div key={index} className={`box-content ${message.type}`}>
           {message.text}
